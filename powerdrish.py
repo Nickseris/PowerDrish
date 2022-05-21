e5054ff3cb018a9a62c9938e387f106fc40116c8
Daniil <82832714+Nickseris@users.noreply.github.com>
2022-05-21 11:20:25 +0300

    Update powerdrish.py
    I have added new levels: tutorial and forest!!!

--- powerdrish.py
+++ powerdrish.py
@@ -4,6 +4,16 @@ font.init()
 
 WHITE = (255, 255, 255)
 
+def reset_level():
+    player.rect.x, player.rect.y = player_x, player_y
+    monsterforrest.rect.x, monsterforrest.rect.y = monster_x, monster_y
+    goal.rect.x, goal.rect.y = goal_x, goal_y
+
+def next_level0():
+    player.rect.x, player.rect.y = player_x, player_y
+    monsterforrest.rect.x, monsterforrest.rect.y = monster_x, monster_y
+    goal.rect.x, goal.rect.y = goal_x, goal_y
+
 # класс-родитель для других спрайтов
 class GameSprite(sprite.Sprite):
  # конструктор класса
@@ -26,11 +36,11 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
     
     def check_goal(self):
-        global run
+        global finish, level
         if self.rect.colliderect(player.rect):
+            finish = True
             window.blit(win, (215, win_height/2))
-
-
+            level = 1
 
 # класс главного игрока
 class Player(GameSprite):
@@ -52,9 +62,9 @@ class Enemy(GameSprite):
     direction_y = "up"
     # горризонтальное передвижение
     def horizontally_update(self, start_pos, end_pos):
-        if self.rect.x <= start_pos:
+        if self.rect.left <= start_pos:
             self.direction_x = "right"
-        if self.rect.x >= end_pos:
+        if self.rect.right >= end_pos:
             self.direction_x = "left"
         if self.direction_x == 'left':
             self.rect.x -= self.speedx
@@ -63,15 +73,21 @@ class Enemy(GameSprite):
 
     # вертикальне передвижение
     def vertically_update(self, start_pos, end_pos):
-        if self.rect.y >= start_pos and self.rect.y <= end_pos:
-            self.rect.y += self.speedy
-        else:
+        if self.rect.top <= start_pos:
+            self.direction_y = "down"
+        if self.rect.bottom >= end_pos:
+            self.direction_y = "up"
+        if self.direction_y == 'up':
             self.rect.y -= self.speedy
+        else:
+            self.rect.y += self.speedy
 
     # смерть
     def kill(self):
-        global run
+        global finish, killing
         if self.rect.colliderect(player.rect):
+            killing = True
+            finish = True
             window.blit(lose, (215, win_height/2))
             
 
@@ -82,7 +98,9 @@ icon = image.load('power.png')
 display.set_icon(icon)
 display.set_caption("PowerDrish")
 window = display.set_mode((win_width, win_height))
+back = transform.scale(image.load("forrest#1.jpg"), (700, 500))
 FPS = 60
+killing = False
 
 
 # текст
@@ -90,14 +108,22 @@ font = font.SysFont(None, 70)
 lose = font.render("You lose ;(", True, (139, 0, 0))
 win = font.render("You win ;)", True, (0, 139, 0))
 
+# местоположение
+player_x, player_y = win_width/2, 400
+monster_x, monster_y = win_width/2, 150
+goal_x, goal_y = win_width/2, 50
+
 # персонажы
-player = Player('drish.png', win_width/2, win_height/2, 60, 60, 6, 6)
-monsterforrest = Enemy('monster.png', win_width/2, 50, 72, 62, 3, 3)
-goal = GameSprite('goal.png', win_width/2, 100, 50, 50, 0, 0)
+player = Player('drish.png', player_x, player_y, 60, 60, 6, 6)
+monsterforrest = Enemy('monster.png', monster_x, monster_y, 72, 62, 3, 3)
+goal = GameSprite('goal.png', goal_x, goal_y, 50, 50, 0, 0)
 ''' должно быть хп тут '''
 
 # Основной цикл игры:
 run = True # флаг сбрасывается кнопкой закрытия окна
+finish = False
+
+level = 0
 
 while run:
 
@@ -106,22 +132,58 @@ while run:
         if e.type == QUIT:
             run = False
 
-    # сама игра: действия спрайтов, проверка правил игры, перерисовка
-    # обновляем фон и персонажей
-    window.fill(WHITE)
-    player.update()
-    monsterforrest.horizontally_update(150, win_width-150)
+    if not finish:
+        
+        # сама игра: действия спрайтов, проверка правил игры, перерисовка
+        if level == 0:
+            # обновляем фон и персонажей
+            window.fill(WHITE)
 
-    # отрисовка
-    monsterforrest.reset()
-    player.reset()
-    goal.reset()
+            player.update()
+            monsterforrest.horizontally_update(0, win_width)
 
-    # проверка на убийство игрока монстром 
-    monsterforrest.kill()
+            # отрисовка
+            monsterforrest.reset()
+            player.reset()
+            goal.reset()
 
-    # проверка: игрока зашел в зону выигрыша?
-    goal.check_goal()
+            # проверка на убийство игрока монстром 
+            monsterforrest.kill()
+
+            # проверка: игрока зашел в зону выигрыша?
+            goal.check_goal()
+
+        if level == 1:
+            # обновляем фон и персонажей
+            window.blit(back, (0, 0))
+
+            # передвижение спрайтов
+            monster_x, monster_y = win_width/2, win_height/2
+            player_x, player_y = 150, win_height/2
+            goal_x, goal_y = win_width-150, win_height/2
+            player.update()
+            monsterforrest.vertically_update(0, win_height)
+
+            # отрисовка
+            monsterforrest.reset()
+            player.reset()
+            goal.reset()
+
+            # проверка на убийство игрока монстром 
+            monsterforrest.kill()
+
+            # проверка: игрока зашел в зону выигрыша?
+            goal.check_goal()
+
+    else:
+        keys = key.get_pressed()
+        if keys[K_SPACE] and killing == True:
+            finish = False
+            reset_level()
+        
+        if keys[K_SPACE] and killing == False:
+            finish = False
+            next_level0()
 
     # FPS
     display.update()
